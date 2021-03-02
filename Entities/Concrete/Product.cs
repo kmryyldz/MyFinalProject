918135050959c39eb8f89cfe157ca6dcca7ecd33
Kumriye Yıldız <kumryldz@gmail.com>
2021-03-02 22:29:23 +0300

    Add project files.

---
+++ Entities/Concrete/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+     public class Product
+    {
+        public int ProductId { get; set; }
+        public int CategoryId { get; set; }
+        public int ProductName { get; set; }
+        public int UnitsInstock { get; set; }
+        public int UnitPrice { get; set; }
+
+    }
+}
